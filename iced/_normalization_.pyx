76274c80e9d6c7671ea889f5b6e0837ceb14296d
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2015-01-08 13:50:25 +0100

    FIX tests now pass

--- iced/_normalization_.pyx
+++ iced/_normalization_.pyx
@@ -20,7 +20,7 @@ def _update_normalization_csr(X, np.ndarray[DOUBLE, ndim=1] bias):
 
     j = 0
     for i, row in enumerate(X_indices):
-        while i > X_indptr[j + 1]:
+        while i >= X_indptr[j + 1]:
             j += 1
         X_data[i] /= bias[row] * bias[j]
     return X
