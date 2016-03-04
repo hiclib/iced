9b7f9220c90710d11d71abb41cec0d7d6c2998ed
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-03-04 23:49:36 +0100

    FIX remove the installation of io as independent module

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ def configuration(parent_package='', top_path=None):
     config = Configuration(None, parent_package, top_path)
 
     config.add_subpackage('iced')
-    config.add_subpackage('iced/io')
 
     return config
 
