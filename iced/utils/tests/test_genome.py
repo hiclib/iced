f62ec42550d6aa7c8a056a77a0f24b9dc10a0437
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-09-07 02:18:56 -0700

    TST added tests for downsample_resolution

--- iced/utils/tests/test_genome.py
+++ iced/utils/tests/test_genome.py
@@ -1,4 +1,5 @@
 import numpy as np
+from nose.tools import assert_equal
 from numpy.testing import assert_array_equal
 
 from iced.utils._genome import get_intra_mask
@@ -6,6 +7,7 @@ from iced.utils._genome import get_inter_mask
 from iced.utils._genome import _change_lengths_resolution
 from iced.utils._genome import undersample_per_chr
 from iced.utils._genome import extract_sub_contact_map
+from iced.utils._genome import downsample_resolution
 
 
 def test_get_intra_mask():
@@ -30,6 +32,26 @@ def test_get_inter_mask():
     true_mask[:5, :5] = 1
     true_mask[5:, 5:] = 1
     assert_array_equal(mask, np.invert(true_mask.astype(bool)))
+
+
+def test_downsample_resolution():
+    random_state = np.random.RandomState(seed=42)
+
+    lengths = np.array([10, 10])
+    counts = np.triu(random_state.randint(
+        0, 100, (lengths.sum(), lengths.sum())))
+    counts = counts + counts.T
+    downsampled_counts, downsampled_lengths = downsample_resolution(
+        counts, lengths)
+    assert_equal(downsampled_lengths.sum(), lengths.sum()/2)
+
+    lengths = np.array([10, 11])
+    counts = np.triu(random_state.randint(
+        0, 100, (lengths.sum(), lengths.sum())))
+    counts = counts + counts.T
+    downsampled_counts, downsampled_lengths = downsample_resolution(
+        counts, lengths)
+    assert_equal(downsampled_lengths.sum(), 11)
 
 
 def test_undersample_per_chr():
