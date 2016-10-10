0dbd8c5253d4fe2e365c58da5ee3407a60c5e654
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2016-10-10 15:45:55 -0700

    Releasing v0.3.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DISTNAME = 'iced'
 DESCRIPTION = 'ICE normalization'
 MAINTAINER = 'Nelle Varoquaux'
 MAINTAINER_EMAIL = 'nelle.varoquaux@gmail.com'
-VERSION = '0.2.3-git'
+VERSION = '0.3.0'
 
 
 def configuration(parent_package='', top_path=None):
