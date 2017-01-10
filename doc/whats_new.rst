87fed8e1c25601b0cf046f1efd897c830870d9a2
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-10 11:46:05 -0800

    DOC added what's new + example

---
+++ doc/whats_new.rst
@@ -0,0 +1,18 @@
+===============
+Release history
+===============
+
+
+Version 0.4
+===========
+
+**In Development**
+
+
+New features
+------------
+
+  - Added a new filtering option `remove_all_zeros_loci` which removes
+    non-interacting rows and columns from the count matrix *prior* to applying
+    the filtering strategy.
+    By Nicolas Servant and Nelle Varoquaux 
--- iced/filter.py
+++ iced/filter.py
@@ -60,37 +60,6 @@ def filter_low_counts(X, lengths=None, percentage=0.02, copy=True,
                                remove_all_zeros_loci=remove_all_zeros_loci)
 
 
-def _filter_low_coverage(X, mincov=5, verbose=False):
-    """
-    Filter rows and columns with low counts
-
-    Parameters
-    ----------
-    X : ndarray (n, n)
-        Count matrix (hollow, symetric)
-
-    mincov : integer, default: 5
-        rows and columns with less counts are discarded
-
-    Return
-    ------
-    X : ndarray (n, n)
-        The filtered array
-    """
-    X_sum = np.array(X.sum(axis=0)).flatten()
-
-    if verbose:
-        print("Filter %s bins ..." % sum(X_sum < mincov))
-
-    if sparse.issparse(X):
-        _filter_csr(X, (X_sum < mincov))
-    else:
-        X[X_sum < mincov, :] = np.nan
-        X[:, X_sum < mincov] = np.nan
-
-    return X
-
-
 def filter_high_counts(X, lengths=None, percentage=0.02, copy=True):
     """
     Filter rows and columns with high counts
