d2df8eb1b28c2ff1ed6c6ae68ddb6db1b2d9de70
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-01-10 16:47:44 -0800

    fix

--- build_tools/travis/test_script.sh
+++ build_tools/travis/test_script.sh
@@ -8,27 +8,49 @@
 
 set -e
 
-# Get into a temp directory to run test from the installed iced and
-# check if we do not leave artifacts
-mkdir -p $TEST_DIR
-# We need the setup.cfg for the nose settings
-cp setup.cfg $TEST_DIR
-cd $TEST_DIR
-
 python --version
 python -c "import numpy; print('numpy %s' % numpy.__version__)"
 python -c "import scipy; print('scipy %s' % scipy.__version__)"
+python -c "\
+try:
+    import pandas
+    print('pandas %s' % pandas.__version__)
+except ImportError:
+    pass
+"
+
 python -c "import multiprocessing as mp; print('%d CPUs' % mp.cpu_count())"
 
-if [[ "$COVERAGE" == "TRUE" ]]; then
-   nosetests -s --with-coverage iced
-else
-   nosetests -s iced
+run_tests(){
+  # Get into a temp directory to run test from the installed iced and
+  # check if we do not leave artifacts
+  mkdir -p $TEST_DIR
+  # We need the setup.cfg for the nose settings
+  cp setup.cfg $TEST_DIR
+  cd $TEST_DIR
+
+
+
+  if [[ "$COVERAGE" == "TRUE" ]]; then
+    nosetests -s --with-coverage iced
+  else
+    nosetests -s iced
+  fi
+
+  # Is directory still empty ?
+  ls -ltra
+
+  # Test doc
+  cd $CACHED_BUILD_DIR/iced
+  make test-doc test-sphinxext
+
+}
+
+
+if [[ "$RUN_FLAKE8" == "true" ]]; then
+    source build_tools/travis/flake8_diff.sh
 fi
 
-# Is directory still empty ?
-ls -ltra
-
-# Test doc
-cd $CACHED_BUILD_DIR/iced
-make test-doc test-sphinxext
+if [[ "$SKIP_TESTS" != "true" ]]; then
+    run_tests
+fi
