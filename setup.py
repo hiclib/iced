bf4025f0e07c4760c5ee74592675a96d9896c543
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2017-06-16 21:07:37 -0700

    Releasing v0.4.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DISTNAME = 'iced'
 DESCRIPTION = 'ICE normalization'
 MAINTAINER = 'Nelle Varoquaux'
 MAINTAINER_EMAIL = 'nelle.varoquaux@gmail.com'
-VERSION = '0.4.1'
+VERSION = '0.4.2'
 
 
 def configuration(parent_package='', top_path=None):
