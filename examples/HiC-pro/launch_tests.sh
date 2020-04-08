801d473a95f31e3ae8744bd59bc9f6ca0eb92ba3
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2020-04-08 10:54:39 +0200

    TST adding test to check results can be properly loaded

--- examples/HiC-pro/launch_tests.sh
+++ examples/HiC-pro/launch_tests.sh
@@ -5,3 +5,5 @@ ice subset.matrix
 ice --results_filename /tmp/iced_matrix.matrix --filter_low_counts_perc 0.02 \
 --filter_high_counts_perc 0.02 --max_iter 1000 --eps 0.1 \
 --remove-all-zeros-loci --output-bias 1 --verbose 1 subset.matrix 
+
+python load_counts.py
