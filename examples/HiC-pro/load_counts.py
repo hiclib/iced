09475a238782bd2b2e273cc8e848edaed37742b6
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2021-07-20 13:55:21 +0200

    Merge pull request #105 from NelleV/bug_off_by_one
    FIX off-by-one in script

--- examples/HiC-pro/load_counts.py
+++ examples/HiC-pro/load_counts.py
@@ -1,3 +1,14 @@
 from iced import io
+import numpy as np
 
 counts = io.load_counts("/tmp/iced_matrix.matrix")
+
+# Load with np.loadtxt and check that the shape makes sense
+print("Checking the shape of the written matrix makes sense")
+t = np.loadtxt("/tmp/iced_matrix.matrix")
+if t.shape[1] != 3:
+    raise ValueError("The shape of the written matrix doesn't make sense")
+
+# Checking that the base seems fine (ie, is one)
+if t[0, 0] < 1 or t[0, 1] < 1:
+    raise ValueError("The output should be 1-based, not 0 based")
