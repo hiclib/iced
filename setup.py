9c3346541ad1c46537dde4d534d7418a6cee88e8
Nelle Varoquaux <nelle.varoquaux@gmail.com>
2021-07-21 14:18:04 +0200

    Releasing v0.5.9
    Fixing saving contact counts in ice

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ DISTNAME = 'iced'
 DESCRIPTION = 'ICE normalization'
 MAINTAINER = 'Nelle Varoquaux'
 MAINTAINER_EMAIL = 'nelle.varoquaux@gmail.com'
-VERSION = '0.5.8'
+VERSION = '0.5.9'
 
 
 SCIPY_MIN_VERSION = '0.13.3'
